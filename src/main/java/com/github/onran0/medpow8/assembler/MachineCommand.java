d9e10e9aeae3058c43c43f745df08a82657f28d1
Onran0 <kotshel2009@gmail.com>
2025-05-01 12:32:10 +0900

    added getOp method to machine command for more flexible

--- src/main/java/com/github/onran0/medpow8/assembler/MachineCommand.java
+++ src/main/java/com/github/onran0/medpow8/assembler/MachineCommand.java
@@ -1,3 +1,12 @@
 package com.github.onran0.medpow8.assembler;
 
-public record MachineCommand(byte command, byte op1, byte op2) { }
+public record MachineCommand(byte command, byte op1, byte op2) {
+
+    public byte getOp(int index) {
+        return switch (index) {
+            case 0 -> op1;
+            case 1 -> op2;
+            default -> -1;
+        };
+    }
+}
