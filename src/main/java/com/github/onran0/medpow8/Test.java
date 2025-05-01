948a2cb25e8f60693fcb7361086e3400579b8737
Onran0 <kotshel2009@gmail.com>
2025-05-01 12:33:42 +0900

    new test and fibonacci script creation

--- src/main/java/com/github/onran0/medpow8/Test.java
+++ src/main/java/com/github/onran0/medpow8/Test.java
@@ -2,44 +2,71 @@ package com.github.onran0.medpow8;
 
 import com.github.onran0.medpow8.assembler.Assembler;
 import com.github.onran0.medpow8.assembler.AssemblyException;
-import com.github.onran0.medpow8.assembler.parser.Command;
+import com.github.onran0.medpow8.assembler.Command;
+import com.github.onran0.medpow8.assembler.MachineCommand;
 import com.github.onran0.medpow8.assembler.parser.Parser;
 import com.github.onran0.medpow8.assembler.token.Token;
 import com.github.onran0.medpow8.assembler.token.Tokenizer;
+import com.github.onran0.medpow8.disassembler.Disassembler;
+import com.github.onran0.medpow8.disassembler.DisassemblyException;
+import com.github.onran0.medpow8.util.IO;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 public class Test {
-    public static void main(String[] args) throws AssemblyException {
-        String code =
-        """
-        mov r0, %5
-        add r0, %r1
-        push r2
-        pop r3
-        hlt
-        """;
+    private static void printBin(byte b) {
+        System.out.print(ensureLen(Integer.toBinaryString(b & 0xFF), 8));
+    }
+
+    private static String ensureLen(String str, int len) {
+        if(str.length() >= len)
+            return str;
+        else
+            return "0".repeat(len - str.length()) + str;
+    }
+
+    public static void main(String[] args) throws AssemblyException, DisassemblyException, IOException {
+        String code = IO.readASMScript("fibonacci.asm");
+
+        System.out.println("Source \n");
+
+        System.out.println(code);
 
         System.out.println("\n Tokens \n");
 
+        List<Token> tokens;
+
         System.out.println(
                 Arrays.toString(
-                        new Tokenizer(code).tokenize().toArray(new Token[0])
+                        (tokens = new Tokenizer(code).tokenize()).toArray(new Token[0])
                 )
         );
 
         System.out.println("\n Commands \n");
 
+        List<Command> commands;
+
         System.out.println(
                 Arrays.toString(
-                        Parser.parse(
-                                new Tokenizer(code).tokenize()
-                        ).toArray(new Command[0])
+                        (commands = Parser.parse(tokens)).toArray(new Command[0])
                 )
         );
 
         System.out.println("\n Machine Code \n");
 
-        System.out.println(Arrays.toString(Assembler.assemble(code)));
+        for(MachineCommand cmd : Assembler.assembleToList(commands)) {
+            System.out.print(ensureLen(String.valueOf(cmd.command() & 0xFF), 3));
+            System.out.print(" ");
+            printBin(cmd.getOp(0));
+            System.out.print(" ");
+            printBin(cmd.getOp(1));
+            System.out.println();
+        }
+
+        System.out.println("\n Disassemble \n");
+
+        System.out.println(Disassembler.disassemble(Assembler.assemble(code)));
     }
 }
